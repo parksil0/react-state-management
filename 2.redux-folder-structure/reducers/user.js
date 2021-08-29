85443fb983264be8d0925891d4196496b0f59ecb
parksil0 <parksil00@naver.com>
2021-08-30 07:28:08 +0900

    reducers 모듈화

---
+++ 2.redux-folder-structure/reducers/user.js
@@ -0,0 +1,23 @@
+const initialState = {
+  isLoggingIn: false,
+  data: null,
+};
+
+const userReducer = (prevState = initialState, action) => {
+  switch (action.type) {
+    case "LOG_IN":
+      return {
+        ...prevState,
+        user: action.data,
+      };
+    case "LOG_OUT":
+      return {
+        ...prevState,
+        user: null,
+      };
+    default:
+      return prevState;
+  }
+};
+
+module.exports = userReducer;
