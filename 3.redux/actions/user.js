50e1f6969edc3c21a2139ef4f7dcf14d057a0bcb
parksil0 <parksil00@naver.com>
2021-09-01 07:38:05 +0900

    user 기능 action, reducer 분리

---
+++ 3.redux/actions/user.js
@@ -0,0 +1,51 @@
+const logIn = () => { // async action creator
+  return (dispatch, getState) => { // async action
+    dispatch(logInRequest());
+    try {
+      setTimeout(() => {
+        dispatch(logInSuccess({
+          userId: 1,
+          nickname: 'SealPark'
+        }));
+      }, 2000);
+    } catch (e) {
+      dispatch(logInFailure(e));
+    }
+  };
+}
+const LOG_IN_REQUEST = "LOG_IN_REQUEST";
+const LOG_IN_SUCCESS = "LOG_IN_SUCCESS";
+const LOG_IN_FAILURE = "LOG_IN_FAILURE";
+const LOG_OUT = "LOG_OUT";
+// const = '';
+const logInRequest = (data) => {
+  return {
+    type: LOG_IN_REQUEST,
+    data,
+  }
+};
+
+const logInSuccess = (data) => {
+  return {
+    type: LOG_IN_SUCCESS,
+    data,
+  }
+};
+
+const logInFailure = (error) => {
+  return {
+    type: LOG_IN_FAILURE,
+    error,
+  }
+};
+
+const logOut = () => {
+  return {
+    type: LOG_OUT,
+  };
+};
+
+module.exports = {
+  logIn,
+  logOut,
+};
