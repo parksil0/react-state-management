a74dfb5635bae72ef4bcd2d9e057206b21ceb410
parksil0 <parksil00@naver.com>
2021-09-02 07:12:02 +0900

    initial commit

---
+++ 4./App.jsx
@@ -0,0 +1,45 @@
+import React, { useCallback, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { logIn, logOut } from "./actions/user";
+
+const App = () => {
+  const user = useSelector((state) => state.user);
+  const posts = useSelector((state) => state.posts);
+  const dispatch = useDispatch();
+
+  const onClick = useCallback(() => {
+    dispatch(
+      logIn({
+        id: "sealPark",
+        password: "1234",
+      })
+    );
+  }, []);
+
+  const onLogout = useCallback(() => {
+    dispatch(logOut());
+  }, []);
+
+  useEffect(() => {
+    console.log(user);
+  }, [user]);
+
+  return (
+    <div>
+      {user.isLoggingIn ? (
+        <div>로그인 중</div>
+      ) : user.data ? (
+        <div>{user.data.nickname}</div>
+      ) : (
+        "로그인 해주세요."
+      )}
+      {!user.data ? (
+        <button onClick={onClick}>로그인</button>
+      ) : (
+        <button onClick={onLogout}>로그아웃</button>
+      )}
+    </div>
+  );
+};
+
+export default App;
---
+++ 4./reducers/user.js
@@ -0,0 +1,33 @@
+const { produce } = require('immer');
+
+const initialState = {
+  isLoggingIn: false,
+  data: null,
+};
+
+const userReducer = (prevState = initialState, action) => {
+  return produce(prevState, (draft) => {
+    switch (action.type) {
+      case "LOG_IN_REQUEST":
+        draft.data = null;
+        draft.isLoggingIn = true;
+        break;
+      case "LOG_IN_SUCCESS":
+        draft.data = action.data;
+        draft.isLoggingIn = false;
+        break;
+      case "LOG_IN_FAILURE":
+        draft.data = null;
+        draft.isLoggingIn = false;
+        break;
+      case "LOG_OUT":
+        draft.data = null;
+        draft.isLoggingIn = false;
+        break;
+      default:
+        break;
+    }
+  });
+};
+
+module.exports = userReducer;
