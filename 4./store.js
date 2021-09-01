2156995af15218b2aea3f62c930407df65d954ec
parksil0 <parksil00@naver.com>
2021-09-02 07:13:59 +0900

    redux-toolkit 라이브러리로 코드 리팩토링

---
+++ 4./store.js
@@ -0,0 +1,21 @@
+const { configureStore, getDefaultMiddleware } = require('@reduxjs/toolkit');
+const reducer = require('./reducers');
+const { logIn, logOut } = require('./actions/user');
+const { addPost } = require('./actions/post');
+
+// custom middleware
+const firstMiddleware = (store) => (next) => (action) => {
+  console.log("action logging", action); // 기능 추가
+  next(action); // 기본 동작
+  // 기능추가
+}
+
+const store = configureStore({
+  reducer,
+  middleware: [firstMiddleware, ...getDefaultMiddleware], // getDefaultMiddleware는 deprecated됨
+  // middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(firstMiddleware),
+  devTools: process.env.NODE_ENV !== 'production',
+  // preloadedState: // ssr 전용
+});
+
+module.exports = store;
